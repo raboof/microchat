193db260bcd4806098ef0ee9c34e4e3ab509baba
Arnout Engelen <arnouten@bzzt.net>
2014-10-24 12:56:24 +0200

    Get user based on sessionId

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"net/http"
 )
 
-func handleUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "{ \"username\": \"raboof\" }")
+func handleUser(user_repo *userrepo.UserRepo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sessionId := r.URL.Query().Get("sessionId")
+		fmt.Fprintf(w, "{ \"username\": \"" + user_repo.FetchUser(sessionId).Name + "\" }");
+	}
 }
 
 func handleUsers(w http.ResponseWriter, r *http.Request) {
@@ -19,13 +22,14 @@ func handleMessages(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{ messages }")
 }
 
+
 func main() {
 	user_repo := userrepo.NewUserRepo()
 	user_repo.StoreUser(userrepo.NewUser("1", "name 1"))
 	user_repo.StoreUser(userrepo.NewUser("2", "name 2"))
 	user_repo.StoreUser(userrepo.NewUser("3", "name 3"))
 
-	http.HandleFunc("/api/user", handleUser)
+	http.HandleFunc("/api/user", handleUser(user_repo))
 	http.HandleFunc("/api/users", handleUsers)
 	http.HandleFunc("/api/messages", handleMessages)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
