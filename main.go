4924f98fd2c66877e61b918c0f8ac88efefaf2e3
Arnout Engelen <arnouten@bzzt.net>
2014-10-24 10:33:39 +0200

    Handler functions for the API calls

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"log"
+	"fmt"
 	"net/http"
 )
 
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{ user }");
+}
+func handleUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{ users }");
+}
+func handleMessages(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{ messages }");
+}
+
 func main() {
+	http.HandleFunc("/api/user", handleUser)
+	http.HandleFunc("/api/users", handleUsers)
+	http.HandleFunc("/api/messages", handleMessages)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("Serving at localhost:8080...")
 	http.ListenAndServe(":8080", nil)
