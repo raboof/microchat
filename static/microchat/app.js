504a32afd89ab1b4b7c5c6218b3fa5e1fa31f9bd
Ruben Oostinga <r.oostinga@fundeon.nl>
2014-10-24 16:20:29 +0200

    update user list

--- static/microchat/app.js
+++ static/microchat/app.js
@@ -22,12 +22,16 @@ $(function () {
     }
   });
 
-  $.ajax(usersUrl, {
-    dataType: 'json',
-    success: function (data) {
-      $('#users').text(data);
-    }
-  });
+  var getUsers = function () {
+    $.ajax(usersUrl, {
+      dataType: 'json',
+      success: function (data) {
+        $('#users').text(data);
+      }
+    });
+  }
+
+  getUsers();
 
   if (!window.location.origin) { // Some browsers (mainly IE) do not have this property, so we need to build it manually...
     window.location.origin = window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '');
@@ -38,12 +42,15 @@ $(function () {
 
   sock.onopen = function () {
     // console.log('connection open');
-    sock.send( GET.sessionId);
+    sock.send(GET.sessionId);
     document.getElementById("status").innerHTML = "connected";
     document.getElementById("send").disabled = false;
   };
 
   sock.onmessage = function (e) {
+    if (e.data.indexOf("[info]") === 0) {
+      getUsers();
+    }
     document.getElementById("output").value += e.data + "\n";
   };
 
