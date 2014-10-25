93636b62fdb00ab8bb9767687e0ab776ffa59043
Marc Grol <mgrol@xebia.com>
2014-10-25 13:48:33 +0200

    script to fetch packages we depend on

---
+++ setup_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+go get github.com/Shopify/sarama
+go get github.com/stretchr/testify/assert
+go get github.com/gorilla/websocket/
+go get github.com/igm/pubsub
+go get gopkg.in/igm/sockjs-go.v2/sockjs
